1ad84b6f8f7b3e3938a8f142123709ed3abf96f1
patricio355 <patricioquiroga35@gmail.com>
2023-07-07 18:47:07 -0300

    no message

--- src/app/components/amazon/amazon.component.ts
+++ src/app/components/amazon/amazon.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AmazonService } from 'src/app/services/amazon.service';
+
 
 @Component({
   selector: 'app-amazon',
@@ -7,45 +7,6 @@ import { AmazonService } from 'src/app/services/amazon.service';
   styleUrls: ['./amazon.component.css']
 })
 export class AmazonComponent {
-  productos! : Array<any>;
-  nombre!: string;
-  asin!:string;
-  review! : string ;
-  constructor (private servicio : AmazonService ){
-    this.productos=new Array<any>();
-  }
-
-  getOp(asin : string){
-    this.servicio.getOtro(this.asin).subscribe(
-      result=>{
-        this.asin = this.productos[0].asin;
-        console.log(result.reviews[0].rating);
-        
-      },
-      error=>{
-
-      }
-
-    )
-  }
-
-  obtenerProducto(){
-
-    for(let i=0;i<=3;i++){
-
-    this.servicio.getDatos(this.nombre).subscribe(
-      result => {
-        this.productos.push({nombre : result.products[i].name , img : result.products[i].image.url, asin : result.products[i].asin });
-    
-      }
-    )
-}
-  }
-
-  
-vaciarArreglo(){
-  this.productos.splice(0, this.productos.length);
-}
 
 }
 
